69e39ce4787e64b9d5746227fe9f9da9d53f4664
Israel Cavalcante <israel.cs@hotmail.com>
2020-05-14 21:12:37 -0300

    modificaçoes

--- Projetos/Testes Pessoais/script.js
+++ Projetos/Testes Pessoais/script.js
@@ -1,11 +1,47 @@
 var visor = document.getElementById("visor")
 var res = document.getElementById("res")
-var resultado, adicao, subtracao, total = 0
-var veriSoma, veriSubt = 0 //variáveis de verificação
+var resultado, adicao, subtracao, multiplicacao, divisao, total, temp = 0
+var veriSoma, veriSubt, veriMult, veriDiv = 0 //variáveis de verificação
 var a = 0
 
+total = document.getElementById('visor').value
+
+// Números
 function but (a) {
     resultado = document.getElementById('visor').value += a
+    total = a
+}
+
+// Operações
+function soma () {
+    veriSoma = 2
+    verificar ()
+    visor = document.getElementById('visor').value = 0
+}
+
+function subt () {  
+    veriSubt = 2
+    verificar ()
+    visor = document.getElementById('visor').value = 0
+}
+
+function mult () {
+    veriMult = 2
+    verificar ()
+    visor = document.getElementById('visor').value = 0
+}
+
+function div () {
+    veriDiv = 2
+    verificar ()
+    visor = document.getElementById('visor').value = 0
+}
+
+//Outras funções
+function igual () {
+    verificar ()
+    res.innerHTML = ''
+    document.getElementById('visor').value = total
 }
 
 function zerar () {
@@ -13,34 +49,39 @@ function zerar () {
     total = 0
 }
 
-function soma () {
-    adicao = parseFloat(document.getElementById('visor').value)
-    total += adicao
-    veriSoma = 1
-    visor = document.getElementById('visor').value = 0
-}
-
-function subt () {
-    subtracao = parseFloat(document.getElementById('visor').value)
-    total -= subtracao
-    veriSubt = 1
-    visor = document.getElementById('visor').value = 0
-}
-
-function igual () {
-    verificar ()
-    res.innerHTML = ''
-    res.innerHTML = `Resultado: ${total}`
-    document.getElementById('visor').value = total
-}
-
+//Verificação
 function verificar () {
-    if (veriSoma === 1) {
-        veriSoma = 0
-        soma ()
-    }else if (veriSubt === 1) {
-        veriSubt = 0
-        subt ()
+    if (veriSoma > 0) {
+        adicao = parseFloat(document.getElementById('visor').value)
+        if (veriSoma === 2) {
+            temp = adicao
+        }
+            total = temp + adicao
+            veriSoma -= 1
+
+    }else if (veriSubt > 0) {
+        subtracao = parseFloat(document.getElementById('visor').value)
+        if (veriSubt === 2) {
+            temp = subtracao
+        }
+        total = temp - subtracao
+        veriSubt -= 1
+
+    }else if (veriMult > 0) {
+        multiplicacao = parseFloat(document.getElementById('visor').value)
+        if (veriMult === 2) {
+            temp = multiplicacao
+        }
+        total = temp * multiplicacao
+        veriMult -= 1
+    }else if (veriDiv > 0) {
+        divisao = parseFloat(document.getElementById('visor').value)
+        if (veriDiv === 2) {
+            temp = divisao
+        }
+        total = temp / divisao
+        veriDiv -= 1
     }
-return null
+ 
 }
+res.innerHTML = `Valor na memória: ${parseFloat(total)}`
